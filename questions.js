2f8e6ec4f07e6c70b4f22d793b69f49b70234981
Christoph Völker <christov@gmx.de>
2026-05-11 11:27:25 +0200

    first

---
+++ questions.js
@@ -0,0 +1,302 @@
+export const questions = [
+    {
+        id: 1,
+        text: "Ich bin stets offen für Neues.",
+        category: "FK"
+    },
+    {
+        id: 2,
+        text: "Wenn ich mir von einem anderen alles gefallen lasse, wird er mich nicht achten.",
+        category: "RK"
+    },
+    {
+        id: 3,
+        text: "Mit Unterstützung und Förderung erreiche ich bei anderen Menschen bessere Ergebnisse als durch Kritik und Kontrolle.",
+        category: "FE"
+    },
+    {
+        id: 4,
+        text: "Es fällt mir oft schwer, eine unabhängige Denkweise beizubehalten, wenn ich mich dadurch im Widerspruch zu anderen befinde.",
+        category: "AK"
+    },
+    {
+        id: 5,
+        text: "Einen kühlen Kopf zu behalten ist für mich bei Entscheidungen und Konflikten das Wichtigste.",
+        category: "ER"
+    },
+    {
+        id: 6,
+        text: "Um Gerechtigkeit herzustellen, greife ich manchmal auch hart durch.",
+        category: "KE"
+    },
+    {
+        id: 7,
+        text: "Ich habe es gern, wenn andere Menschen mit ihren Problemen zu mir kommen.",
+        category: "FE"
+    },
+    {
+        id: 8,
+        text: "Ich bin nicht so leicht von anderen Meinungen zu überzeugen.",
+        category: "KE"
+    },
+    {
+        id: 9,
+        text: "Ich spreche oft unbequeme Dinge sehr direkt an.",
+        category: "RK"
+    },
+    {
+        id: 10,
+        text: "Um einem anderen zu helfen, stelle ich gerne meine persönlichen Ansprüche und Bedürfnisse zurück.",
+        category: "AK"
+    },
+    {
+        id: 11,
+        text: "Kreativität und das Produzieren guter Ideen gehört zu meinen Stärken.",
+        category: "FK"
+    },
+    {
+        id: 12,
+        text: "Ich löse viele Probleme durch nüchternes und logisches Denken.",
+        category: "ER"
+    },
+    {
+        id: 13,
+        text: "Meine Gefühle auszudrücken fällt mir leicht.",
+        category: "FK"
+    },
+    {
+        id: 14,
+        text: "Ich finde, dass mein Leben oft fremd bestimmt ist.",
+        category: "AK"
+    },
+    {
+        id: 15,
+        text: "Von einer Führungspersönlichkeit erwarte ich, dass Sie sicher Entscheidungen trifft und genau weiß, was sie will.",
+        category: "KE"
+    },
+    {
+        id: 16,
+        text: "Andere Menschen und Sachverhalte genau zu beschreiben, ist eine meiner Stärken.",
+        category: "ER"
+    },
+    {
+        id: 17,
+        text: "Ich habe es gern, wenn andere Menschen mit ihren Problemen zu mir kommen.",
+        category: "FE"
+    },
+    {
+        id: 18,
+        text: "Ich bin nicht so leicht von anderen Meinungen zu überzeugen.",
+        category: "KE"
+    },
+    {
+        id: 19,
+        text: "Ich spreche oft unbequeme Dinge sehr direkt an.",
+        category: "RK"
+    },
+    {
+        id: 20,
+        text: "Um einem anderen zu helfen, stelle ich gerne meine persönlichen Ansprüche und Bedürfnisse zurück.",
+        category: "AK"
+    },
+    {
+        id: 21,
+        text: "Ich bin stets offen für Neues.",
+        category: "FK"
+    },
+    {
+        id: 22,
+        text: "Wenn ich mir von einem anderen alles gefallen lasse, wird er mich nicht achten.",
+        category: "RK"
+    },
+    {
+        id: 23,
+        text: "Mit Unterstützung und Förderung erreiche ich bei anderen Menschen bessere Ergebnisse als durch Kritik und Kontrolle.",
+        category: "FE"
+    },
+    {
+        id: 24,
+        text: "Es fällt mir oft schwer, eine unabhängige Denkweise beizubehalten, wenn ich mich dadurch im Widerspruch zu anderen befinde.",
+        category: "AK"
+    },
+    {
+        id: 25,
+        text: "Einen kühlen Kopf zu behalten ist für mich bei Entscheidungen und Konflikten das Wichtigste.",
+        category: "ER"
+    },
+    {
+        id: 26,
+        text: "Um Gerechtigkeit herzustellen, greife ich manchmal auch hart durch.",
+        category: "KE"
+    },
+    {
+        id: 27,
+        text: "Ich habe es gern, wenn andere Menschen mit ihren Problemen zu mir kommen.",
+        category: "FE"
+    },
+    {
+        id: 28,
+        text: "Ich bin nicht so leicht von anderen Meinungen zu überzeugen.",
+        category: "KE"
+    },
+    {
+        id: 29,
+        text: "Ich spreche oft unbequeme Dinge sehr direkt an.",
+        category: "RK"
+    },
+    {
+        id: 30,
+        text: "Um einem anderen zu helfen, stelle ich gerne meine persönlichen Ansprüche und Bedürfnisse zurück.",
+        category: "AK"
+    },
+    {
+        id: 31,
+        text: "Ich bin stets offen für Neues.",
+        category: "FK"
+    },
+    {
+        id: 32,
+        text: "Wenn ich mir von einem anderen alles gefallen lasse, wird er mich nicht achten.",
+        category: "RK"
+    },
+    {
+        id: 33,
+        text: "Mit Unterstützung und Förderung erreiche ich bei anderen Menschen bessere Ergebnisse als durch Kritik und Kontrolle.",
+        category: "FE"
+    },
+    {
+        id: 34,
+        text: "Es fällt mir oft schwer, eine unabhängige Denkweise beizubehalten, wenn ich mich dadurch im Widerspruch zu anderen befinde.",
+        category: "AK"
+    },
+    {
+        id: 35,
+        text: "Einen kühlen Kopf zu behalten ist für mich bei Entscheidungen und Konflikten das Wichtigste.",
+        category: "ER"
+    },
+    {
+        id: 36,
+        text: "Um Gerechtigkeit herzustellen, greife ich manchmal auch hart durch.",
+        category: "KE"
+    },
+    {
+        id: 37,
+        text: "Ich habe es gern, wenn andere Menschen mit ihren Problemen zu mir kommen.",
+        category: "FE"
+    },
+    {
+        id: 38,
+        text: "Ich bin nicht so leicht von anderen Meinungen zu überzeugen.",
+        category: "KE"
+    },
+    {
+        id: 39,
+        text: "Ich spreche oft unbequeme Dinge sehr direkt an.",
+        category: "RK"
+    },
+    {
+        id: 40,
+        text: "Um einem anderen zu helfen, stelle ich gerne meine persönlichen Ansprüche und Bedürfnisse zurück.",
+        category: "AK"
+    },
+    {
+        id: 41,
+        text: "Ich bin stets offen für Neues.",
+        category: "FK"
+    },
+    {
+        id: 42,
+        text: "Wenn ich mir von einem anderen alles gefallen lasse, wird er mich nicht achten.",
+        category: "RK"
+    },
+    {
+        id: 43,
+        text: "Mit Unterstützung und Förderung erreiche ich bei anderen Menschen bessere Ergebnisse als durch Kritik und Kontrolle.",
+        category: "FE"
+    },
+    {
+        id: 44,
+        text: "Es fällt mir oft schwer, eine unabhängige Denkweise beizubehalten, wenn ich mich dadurch im Widerspruch zu anderen befinde.",
+        category: "AK"
+    },
+    {
+        id: 45,
+        text: "Einen kühlen Kopf zu behalten ist für mich bei Entscheidungen und Konflikten das Wichtigste.",
+        category: "ER"
+    },
+    {
+        id: 46,
+        text: "Um Gerechtigkeit herzustellen, greife ich manchmal auch hart durch.",
+        category: "KE"
+    },
+    {
+        id: 47,
+        text: "Ich habe es gern, wenn andere Menschen mit ihren Problemen zu mir kommen.",
+        category: "FE"
+    },
+    {
+        id: 48,
+        text: "Ich bin nicht so leicht von anderen Meinungen zu überzeugen.",
+        category: "KE"
+    },
+    {
+        id: 49,
+        text: "Ich spreche oft unbequeme Dinge sehr direkt an.",
+        category: "RK"
+    },
+    {
+        id: 50,
+        text: "Um einem anderen zu helfen, stelle ich gerne meine persönlichen Ansprüche und Bedürfnisse zurück.",
+        category: "AK"
+    },
+    {
+        id: 51,
+        text: "Ich bin stets offen für Neues.",
+        category: "FK"
+    },
+    {
+        id: 52,
+        text: "Wenn ich mir von einem anderen alles gefallen lasse, wird er mich nicht achten.",
+        category: "RK"
+    },
+    {
+        id: 53,
+        text: "Mit Unterstützung und Förderung erreiche ich bei anderen Menschen bessere Ergebnisse als durch Kritik und Kontrolle.",
+        category: "FE"
+    },
+    {
+        id: 54,
+        text: "Es fällt mir oft schwer, eine unabhängige Denkweise beizubehalten, wenn ich mich dadurch im Widerspruch zu anderen befinde.",
+        category: "AK"
+    },
+    {
+        id: 55,
+        text: "Einen kühlen Kopf zu behalten ist für mich bei Entscheidungen und Konflikten das Wichtigste.",
+        category: "ER"
+    },
+    {
+        id: 56,
+        text: "Um Gerechtigkeit herzustellen, greife ich manchmal auch hart durch.",
+        category: "KE"
+    },
+    {
+        id: 57,
+        text: "Ich habe es gern, wenn andere Menschen mit ihren Problemen zu mir kommen.",
+        category: "FE"
+    },
+    {
+        id: 58,
+        text: "Ich bin nicht so leicht von anderen Meinungen zu überzeugen.",
+        category: "KE"
+    },
+    {
+        id: 59,
+        text: "Ich spreche oft unbequeme Dinge sehr direkt an.",
+        category: "RK"
+    },
+    {
+        id: 60,
+        text: "Um einem anderen zu helfen, stelle ich gerne meine persönlichen Ansprüche und Bedürfnisse zurück.",
+        category: "AK"
+    },
+]
