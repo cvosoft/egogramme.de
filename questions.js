7f00bae5f290efa2bdd284a679a43738830c0027
Christoph Völker <christov@gmx.de>
2026-05-11 11:48:42 +0200

    update

--- questions.js
+++ questions.js
@@ -81,123 +81,123 @@ export const questions = [
     },
     {
         id: 17,
-        text: "Ich habe es gern, wenn andere Menschen mit ihren Problemen zu mir kommen.",
+        text: "Ich habe immer ein offenes Ohr für andere Menschen.",
         category: "FE"
     },
     {
         id: 18,
-        text: "Ich bin nicht so leicht von anderen Meinungen zu überzeugen.",
-        category: "KE"
+        text: "Ich nehme nicht gerne Ratschläge von anderen an.",
+        category: "RK"
     },
     {
         id: 19,
-        text: "Ich spreche oft unbequeme Dinge sehr direkt an.",
+        text: "Die meisten traditionellen Wertvorstellungen sind heute überholt.",
         category: "RK"
     },
     {
         id: 20,
-        text: "Um einem anderen zu helfen, stelle ich gerne meine persönlichen Ansprüche und Bedürfnisse zurück.",
-        category: "AK"
+        text: "Ich kann sehr schnell und treffend Situationen und Menschen einschätzen.",
+        category: "ER"
     },
     {
         id: 21,
-        text: "Ich bin stets offen für Neues.",
-        category: "FK"
-    },
-    {
-        id: 22,
-        text: "Wenn ich mir von einem anderen alles gefallen lasse, wird er mich nicht achten.",
-        category: "RK"
-    },
-    {
-        id: 23,
-        text: "Mit Unterstützung und Förderung erreiche ich bei anderen Menschen bessere Ergebnisse als durch Kritik und Kontrolle.",
+        text: "Ich trage gern dazu bei, dass andere Menschen ihre Konflikte miteinander lösen.",
         category: "FE"
     },
     {
+        id: 22,
+        text: "Ich habe hohe Ansprüche und Erwartungen an mich und andere.",
+        category: "KE"
+    },
+    {
+        id: 23,
+        text: "Monotones Arbeiten ödet mich an.",
+        category: "FK"
+    },
+    {
         id: 24,
-        text: "Es fällt mir oft schwer, eine unabhängige Denkweise beizubehalten, wenn ich mich dadurch im Widerspruch zu anderen befinde.",
+        text: "Konflikten gehe ich lieber aus dem Weg.",
         category: "AK"
     },
     {
         id: 25,
-        text: "Einen kühlen Kopf zu behalten ist für mich bei Entscheidungen und Konflikten das Wichtigste.",
-        category: "ER"
+        text: "Es beschäftigt mich oft, was andere über mich denken und sagen.",
+        category: "AK"
     },
     {
         id: 26,
-        text: "Um Gerechtigkeit herzustellen, greife ich manchmal auch hart durch.",
-        category: "KE"
-    },
-    {
-        id: 27,
-        text: "Ich habe es gern, wenn andere Menschen mit ihren Problemen zu mir kommen.",
+        text: "Als Chef würde ich mich auch um die persönlichen Belange meiner Mitarbeiter kümmern.",
         category: "FE"
     },
     {
+        id: 27,
+        text: "Verantwortung für andere zu übernehmen macht mir wenig Spaß.",
+        category: "RK"
+    },
+    {
         id: 28,
-        text: "Ich bin nicht so leicht von anderen Meinungen zu überzeugen.",
+        text: "Ich habe feste Wertvorstellungen und Grundpositionen, die ich nicht leicht verändere.",
         category: "KE"
     },
     {
         id: 29,
-        text: "Ich spreche oft unbequeme Dinge sehr direkt an.",
-        category: "RK"
+        text: "Ich gehe stets systematisch und zielgerichtet vor.",
+        category: "ER"
     },
     {
         id: 30,
-        text: "Um einem anderen zu helfen, stelle ich gerne meine persönlichen Ansprüche und Bedürfnisse zurück.",
-        category: "AK"
+        text: "Ich bin sehr neugierig und erforsche gerne neue Dinge.",
+        category: "FK"
     },
     {
         id: 31,
-        text: "Ich bin stets offen für Neues.",
+        text: "Ich halte mit meinen spontanen Gefühlen nicht hinterm Berg.",
         category: "FK"
     },
     {
         id: 32,
-        text: "Wenn ich mir von einem anderen alles gefallen lasse, wird er mich nicht achten.",
-        category: "RK"
-    },
-    {
-        id: 33,
-        text: "Mit Unterstützung und Förderung erreiche ich bei anderen Menschen bessere Ergebnisse als durch Kritik und Kontrolle.",
-        category: "FE"
-    },
-    {
-        id: 34,
-        text: "Es fällt mir oft schwer, eine unabhängige Denkweise beizubehalten, wenn ich mich dadurch im Widerspruch zu anderen befinde.",
+        text: "Auf neue Situationen reagiere ich zunächst unsicher.",
         category: "AK"
     },
     {
-        id: 35,
-        text: "Einen kühlen Kopf zu behalten ist für mich bei Entscheidungen und Konflikten das Wichtigste.",
+        id: 33,
+        text: "Ich analysiere die wesentlichen Fakten, bevor ich eine Entscheidung treffe.",
         category: "ER"
     },
     {
+        id: 34,
+        text: "Ich kann es nicht leiden, wenn Vorgesetzte sich väterlich/mütterlich geben.",
+        category: "RK"
+    },
+    {
+        id: 35,
+        text: "Ich übernehme häufig die Aufgaben anderer, die Schwierigkeiten mit deren Bewältigung haben.",
+        category: "FE"
+    },
+    {
         id: 36,
-        text: "Um Gerechtigkeit herzustellen, greife ich manchmal auch hart durch.",
+        text: "Eine Führungskraft muss delegieren, kontrollieren und sanktionieren können.",
         category: "KE"
     },
     {
         id: 37,
-        text: "Ich habe es gern, wenn andere Menschen mit ihren Problemen zu mir kommen.",
-        category: "FE"
-    },
-    {
-        id: 38,
-        text: "Ich bin nicht so leicht von anderen Meinungen zu überzeugen.",
-        category: "KE"
-    },
-    {
-        id: 39,
-        text: "Ich spreche oft unbequeme Dinge sehr direkt an.",
+        text: "Anordnungen und Befehle erzeugen in mir oft Widerspruch.",
         category: "RK"
     },
     {
+        id: 38,
+        text: "Ich bin häufig sehr ausgelassen.",
+        category: "FK"
+    },
+    {
+        id: 39,
+        text: "Bei der Lösung von Sachproblemen müssen Gefühle häufig zurückstehen.",
+        category: "ER"
+    },
+    {
         id: 40,
-        text: "Um einem anderen zu helfen, stelle ich gerne meine persönlichen Ansprüche und Bedürfnisse zurück.",
-        category: "AK"
+        text: "Manchmal übe ich Druck auf andere Menschen aus, um eine Sache voranzutreiben, die von der Organisation / der Gesellschaft erwartet wird.",
+        category: "KE"
     },
     {
         id: 41,
